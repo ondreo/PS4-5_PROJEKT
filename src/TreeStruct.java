551fcecc8191bde52e704fe78d1abaf6274e4b7d
ondreo <65464616+ondreo@users.noreply.github.com>
2020-12-15 04:13:27 +0100

    zła wersja tablicowa

--- src/TreeStruct.java
+++ src/TreeStruct.java
@@ -1,31 +1,67 @@
+import java.util.NoSuchElementException;
+
 public class TreeStruct {
     private TreeVertex startVertex;
-    private TreeVertex[] vertices;//a może lista??
+    //private TreeVertex[] vertices;//a może lista??
     public TreeStruct() {
         startVertex = null;
-        vertices = new TreeVertex[Main.MAX_N];
+        //vertices = new TreeVertex[Main.MAX_N+1];
+    }
+
+    public int findCity(String name) {
+        int currVertex = 0;
+        while(vertices[currVertex] != null) {
+            if(name.compareTo(vertices[currVertex].getName()) < 0) {
+                currVertex *= 2;
+            }
+            else if(name.compareTo(vertices[currVertex].getName()) > 0) {
+                currVertex = currVertex * 2 + 1;
+            }
+            else {
+                return currVertex;
+            }
+        }
+        throw new NoSuchElementException("There is no city with that name.");//Check it's name again.
     }
     //public void add(TreeVertex x) {
     public void add(String name) throws NameException {
-        TreeVertex currVertex = startVertex;
-        while(currVertex != null) {
+        int currVertex = 0;
+        while(vertices[currVertex] != null) {
             /**
              * wstawiany wierzchołek jest mniejszy leksykograficznie od aktualnego
              */
-            if(name.compareTo(currVertex.getName()) < 0) {
-                currVertex = currVertex.getLeftChild();
+            if(name.compareTo(vertices[currVertex].getName()) < 0) {
+                currVertex *= 2;
             }
-            else if(name.compareTo(currVertex.getName()) > 0) {
-                currVertex = currVertex.getRightChild();
+            else if(name.compareTo(vertices[currVertex].getName()) > 0) {
+                currVertex = currVertex * 2 + 1;
             }
             else {
                 throw new NameException("Given city name already exists!");
             }
         }
-        currVertex = new TreeVertex(name);//jak dodać nowy?
+        vertices[currVertex] = new TreeVertex(name);//jak dodać nowy?
     }
+    public void remove(String name) {
+        int currVertex;
+        try {
+            currVertex = this.findCity(name);
+        }
+        catch (NoSuchElementException e) {
+            throw new NoSuchElementException(e.getMessage());
+        }
+        //sprawdzić obsługę przez wyjątek
+        if(vertices[currVertex*2] == null && vertices[currVertex*2+1] == null) {
+            vertices[currVertex] = null;
+        }
+        else if(vertices[currVertex*2] == null) {
+            vertices
+        }
+    }
+
+
     private Boolean isEmpty() {
-        if(startVertex == null)
+        if(vertices[0] == null)
             return true;
         return false;
     }
--- src/TreeVertex.java
+++ src/TreeVertex.java
@@ -1,31 +1,31 @@
 public class TreeVertex {
     private String name;
-    private TreeVertex leftChild;
-    private TreeVertex rightChild;
+    //private TreeVertex leftChild;
+    //private TreeVertex rightChild;
     public TreeVertex(String name) {
         this.name = name;
-        leftChild = rightChild = null;
+        //leftChild = rightChild = null;
     }
 
     public TreeVertex(TreeVertex x) {
         this.name = x.name;
-        this.leftChild = x.leftChild;
-        this.rightChild = x.rightChild;
+        //this.leftChild = x.leftChild;
+        //this.rightChild = x.rightChild;
     }
-    public Boolean hasChildren() {
+    /*public Boolean hasChildren() {
         if(leftChild != null || rightChild != null)
             return true;
         return false;
-    }
+    }*/
 
     public String getName() {
         return name;
     }
-    public TreeVertex getLeftChild() {
+    /*public TreeVertex getLeftChild() {
         return leftChild;
     }
 
     public TreeVertex getRightChild() {
         return rightChild;
-    }
+    }*/
 }
